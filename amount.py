cdf19a5a8753819585525a319fb256381f716ad0
danial <danialsajedian.com>
2024-06-20 15:06:42 +0330

    first commit

---
+++ amount.py
@@ -0,0 +1,25 @@
+amount = eval(input("Yek add sahih vared konid: "))
+
+numberof50 = amount // 50
+    
+amount1 = amount % 50
+
+print("\nTedad mojod to add (50) :",numberof50,"\nBaghiMande:",amount1)
+
+numberof25 = amount1 // 25
+
+amount2 = amount1 % 25
+
+print("\nTedad mojod to add (25) :",numberof25,"\nBaghiMande:",amount2)
+
+numberof20 = amount2 // 20
+
+amount3 = amount2 % 20
+
+print("\nTedad mojod to add (20) :",numberof20,"\nBaghiMande:",amount3)
+
+numberof10 = amount3 // 10
+
+amount4 = amount3 % 10
+
+print("\nTedad mojod to add (10) :",numberof10,"\nBaghiMande:",amount4)
